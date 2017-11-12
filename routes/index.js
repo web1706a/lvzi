f4a85515568bebfe9999d59891b2322c87dc502b
LiWenhua <liwenhua@tedu.cn>
2017-11-12 15:36:53 +0800

    ADD public/index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('<h3>WELCOME TO LVZI</h3>');
+  res.end();
 });
 
 module.exports = router;
