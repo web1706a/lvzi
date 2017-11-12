6a260abaf139a0150802200e3d9aa3ce8a2d2da6
LiWenhua <liwenhua@tedu.cn>
2017-11-12 15:27:28 +0800

    remove jade template engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<h3>WELCOME TO LVZI</h3>');
 });
 
 module.exports = router;
